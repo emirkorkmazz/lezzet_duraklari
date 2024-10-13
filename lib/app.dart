b9226a34fa8dfc72e37041a7af30e94e0295eb10
emirkorkmazz <emirkorkmaz000@gmail.com>
2024-10-13 20:02:45 +0300

    Lint paketi very_good_analysis eklendi ve lint kuralları yapılandırıldı

---
+++ lib/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const _MyHomePage(),
+    );
+  }
+}
+
+class _MyHomePage extends StatefulWidget {
+  const _MyHomePage({super.key});
+
+  @override
+  State<_MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<_MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Lezzet Durakları'),
+      ),
+      body: const Center(
+        child: Text(
+          'Lezzet Durakları',
+        ),
+      ),
+    );
+  }
+}
